e8afc375fbdcb00f9be3885126553a2a82de0823
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-19 19:11:51 +0200

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -16,5 +16,6 @@ afterAll(async () => {
 describe('[GET] /hobbits', () => {
   it('should return a 200 OK status', async () => {
     const res = await request(server).get('/hobbits')
+    expect(res.status).toBe(200)
   })
 })
