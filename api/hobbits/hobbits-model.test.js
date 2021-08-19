94b5675766a9f3b07828a679b65d60550640b146
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-19 18:24:25 +0200

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,3 +1,7 @@
+const Hobbit = require('./hobbits-model')
+
 test('it is the correct environment for the tests', () => {
   expect(process.env.DB_ENV).toBe('development')
 })
+
+describe('Hobbit db access functions')
